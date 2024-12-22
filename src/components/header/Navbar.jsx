811693abd367d3a8dca97b1e10ef1cc622c04450
jayanth-14 <jayanthpetchetti@gmail.com>
2024-12-22 07:41:53 +0530

    change navbar links

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { FaBars, FaSearch } from 'react-icons/fa';
+import { NavLink } from 'react-router';
 
 function Categories() {
-  return(
+  return (
     <div className="px-3 py-2 flex flex-row items-center gap-2 cursor-pointer">
       <p className="text-lg">Categories</p>
       <FaBars />
@@ -11,23 +12,31 @@ function Categories() {
 }
 
 function SearchBar() {
-  return(
-    <div className="px-3 py-2 flex flex-row gap-2 items-center">
+  return (
+    <div className="px-3 py-2 flex flex-row gap-2 items-center w-80">
       <FaSearch />
-      <input type="text" className='bg-transparent border-b-2 border-black outline-none' />
+      <input type="text" className='bg-transparent border-b-2 border-black outline-none w-full' />
     </div>
   )
 }
 
 function Navbar() {
   return (
-    <nav className='container flex flex-row justify-between items-center  w-full bg-blueTwo px-2 py-3'>
-      <Categories />
+    <nav className='container flex flex-row justify-between items-center  w-full bg-blueTwo px-20 py-3'>
+      {/* <Categories /> */}
       <ul className='flex flex-row items-center justify-center gap-8'>
-        <li className='text-lg cursor-pointer hover:text-white '>Home</li>
-        <li className='text-lg cursor-pointer hover:text-white '>About</li>
-        <li className='text-lg cursor-pointer hover:text-white '>Contact</li>
-        <li className='text-lg cursor-pointer hover:text-white '>Services</li>
+        <li className='text-lg cursor-pointer hover:text-white '>
+          <NavLink to="/" className={({ isActive }) => (isActive && "font-semibold text-white" )}>Home</NavLink>
+        </li>
+        <li className='text-lg cursor-pointer hover:text-white '>
+          <NavLink to="/explore">Explore</NavLink>
+        </li>
+        <li className='text-lg cursor-pointer hover:text-white '>
+          <NavLink to="/search">Search</NavLink>
+        </li>
+        <li className='text-lg cursor-pointer hover:text-white '>
+          <NavLink to="/profile">Profile</NavLink>
+        </li>
       </ul>
       <SearchBar />
     </nav>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,15 +4,11 @@ import HeroLeft from './HeroLeft'
 
 function Hero() {
   return (
-    <section className="relative bg-gradient-to-br from-blueOne to-blueTwo text-white py-16 px-8">
+    <section className="relative bg-gradient-to-br from-blueOne to-blueTwo text-white py-16 px-20">
       <div className="container mx-auto flex flex-col md:flex-row items-center">
         <HeroLeft />
         <HeroRight />
       </div>
-
-      {/* Decorative Shapes */}
-      <div className="absolute top-10 left-10 bg-blueThree rounded-full w-20 h-20 opacity-50 blur-xl"></div>
-      <div className="absolute bottom-10 right-10 bg-blueTwo rounded-full w-32 h-32 opacity-50 blur-2xl"></div>
     </section>
   );
 };
