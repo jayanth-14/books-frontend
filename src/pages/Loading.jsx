4842e89f2ede2f2baaf2327c25f80f0feb3fcc66
Swaroopp15 <swarooppalacharla05@gmail.com>
2025-01-07 21:47:48 +0530

    added LoadingPage component for loading state indication

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+  return (
+    <div className="fixed inset-0 bg-white bg-opacity-50 flex items-center justify-center z-50">
+      <div className="w-12 h-12 border-4 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default LoadingPage;
