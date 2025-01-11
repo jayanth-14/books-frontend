c4aa0869122fd88459e4a40eafe5cdeb8b5c63c5
jayanth-14 <jayanthpetchetti@gmail.com>
2025-01-11 11:10:27 +0530

    update book card

--- src/components/books/BookCard.jsx
+++ src/components/books/BookCard.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
+import { Link } from "react-router";
+import ButtonPrimary from "../buttons/buttonPrimary";
 
-function BookCard({book}) {
+function BookCard({book, index}) {
   return (
-    <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-      <a className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
-        <img className="object-cover object-center" src="./images/dummy_data/book_1.jpg" alt="product image" />
-      </a>
-      <div className="mt-4 px-5 pb-5">
-        <a href="#">
-          <h5 className="text-xl tracking-tight text-slate-900">{book.title}</h5>
-        </a>
-        <div className="mt-2 mb-5 flex items-center justify-between">
-          <p>
-            <span className="text-3xl font-bold text-slate-900">by {book.author}</span>
-          </p>
-          <div className="flex items-center">
-            <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">{book.price}</span>
-          </div>
-        </div>
-        <a href="#" className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-          <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
-          View Details
-          </a>
+    <div
+    key={index}
+    className="bg-white rounded-lg p-2 sm:p-4 shadow-xl flex flex-col hover:shadow-2xl transition-shadow duration-300"
+  >
+    <div className="relative pb-[120%] mb-4">
+      <img
+        src={`./images/dummy_data/book_${index + 1}.jpg`}
+        alt={`${book.title}`}
+        className="absolute top-0 left-0 w-full h-full object-cover rounded-xl p-2"
+      />
+    </div>
+    <div className="flex flex-col flex-grow">
+      <h1 className="text-base sm:text-lg md:text-xl font-semibold text-neutral-900 capitalize line-clamp-2">{book.title}</h1>
+      <p className="text-xs sm:text-sm md:text-base font-normal text-gray-600 mb-1">by {book.author}</p>
+      <p className="text-sm sm:text-md md:text-lg font-medium text-blue-500 mb-1">$ {book.price}</p>
+      <div className="mt-auto">
+        <Link to={`/book/${book._id}`} className="block">
+          <ButtonPrimary className="w-full text-xs sm:text-sm md:text-base">View Details</ButtonPrimary>
+        </Link>
       </div>
     </div>
+  </div>
   )
 }
 
--- src/components/books/BookList.jsx
+++ src/components/books/BookList.jsx
@@ -1,32 +1,15 @@
+import BookCard from "./BookCard";
+
 function BookList({ data }) {
   return (
     <div className="container mx-auto px-2 sm:px-4 py-4 sm:py-8">
       <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 sm:gap-4 md:gap-6">
         {data.map((book, index) => (
-          <div
-            key={index}
-            className="bg-white rounded-lg p-2 sm:p-4 shadow-xl flex flex-col hover:shadow-2xl transition-shadow duration-300"
-          >
-            <div className="relative pb-[120%] mb-4">
-              <img
-                src={`./images/dummy_data/book_${index + 1}.jpg`}
-                alt={`${book.title}`}
-                className="absolute top-0 left-0 w-full h-full object-cover rounded-xl p-2"
-              />
-            </div>
-            <div className="flex flex-col flex-grow">
-              <h1 className="text-base sm:text-lg md:text-xl font-semibold text-neutral-900 capitalize line-clamp-2">{book.title}</h1>
-              <p className="text-xs sm:text-sm md:text-base font-normal text-gray-600 mb-1">by {book.author}</p>
-              <p className="text-sm sm:text-md md:text-lg font-medium text-blue-500 mb-1">$ {book.price}</p>
-              <div className="mt-auto">
-                <Link to={`/book/${book.id}`} className="block">
-                  <ButtonPrimary className="w-full text-xs sm:text-sm md:text-base">View Details</ButtonPrimary>
-                </Link>
-              </div>
-            </div>
-          </div>
+         <BookCard book={book} key={index} index={index} />
         ))}
       </div>
     </div>
   );
 }
+
+export default BookList;
