ddbf5df952cafb01e902b1bc88bad9ec071ee405
jayanth-14 <jayanthpetchetti@gmail.com>
2025-01-19 13:52:12 +0530

    updat book details page buttons

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -4,6 +4,11 @@ import { Context } from '../data/states'
 import { useParams } from 'react-router'
 import LoadingPage from './Loading'
 import useGet from '../hooks/useGet'
+import { CgShoppingCart } from 'react-icons/cg'
+import { FaCartShopping } from 'react-icons/fa6'
+import { HiHeart } from 'react-icons/hi'
+import { BsHeartHalf } from 'react-icons/bs'
+import { BiHeart } from 'react-icons/bi'
 
 function Book() {
   const { id } = useParams()
@@ -48,11 +53,9 @@ function Book() {
                 </div>
                 <div className="flex">
                   <span className="title-font font-medium text-2xl text-gray-900">₹ {book?.price}</span>
-                  <button className="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded">Button</button>
-                  <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                    </svg>
+                  <button className="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded">Buy Now</button>
+                  <button className="rounded-md  h-10 bg-gray-200 p-3 border-0 inline-flex items-center justify-start text-gray-500 ml-4 hover:bg-gray-700 hover:text-gray-100">
+                    <HiHeart className='text-2xl' /> <span className='text-base'>Add to Wishlist</span>
                   </button>
                 </div>
               </div>
