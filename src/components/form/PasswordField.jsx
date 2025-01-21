50a2cab6b862b0df6543f7d3701fc878a726177e
Vamsiram07 <vamsiramsalla729@gmail.com>
2025-01-21 15:28:55 +0530

    i have add the feature to display the password

--- src/components/form/PasswordField.jsx
+++ src/components/form/PasswordField.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { FaEye } from 'react-icons/fa'
 
 function PasswordField(params) {
+  const field=useRef(null) 
+  function displaypassword(){
+    if(field.current.type ==="password"){
+      field.current.type= "text"
+      return 
+    }
+    field.current.type="password"
+  }
   return (
     <div className="passwordField">
       <label className='flex flex-col gap-1'> {params.title} :
         <div className='rounded bg-gray-200 w-full h-8 px-2 flex flex-row justify-between items-center'>
-          <input type="password" placeholder={`${params.title}`} className='outline-none bg-transparent w-full' minlength="6" onChange={(event) => params.onChange(event.target.value)} required/>
-          <FaEye className='cursor-pointer' />
+          <input type="password" placeholder={`${params.title}`} className='outline-none bg-transparent w-full' minlength="6" onChange={(event) => params.onChange(event.target.value)} required ref={field}/>
+          <FaEye onClick={displaypassword} className='cursor-pointer' />
         </div>
       </label>
     </div>
