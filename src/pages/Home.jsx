15d6769989be1417937fbcaab2939aa1f4c20e5a
jayanth-14 <jayanthpetchetti@gmail.com>
2025-02-15 05:28:45 +0530

    modify home page image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,21 +7,21 @@ const Home = () => {
   const { isLogined } = useContext(Context);
   return (
     <div
-      className="h-screen px-32 flex flex-row items-center justify-between text-white bg-cover bg-center"
-      style={{ backgroundImage: "url('./public/hero.png')" }}
+    className="h-screen px-32 flex flex-row items-center gap-10 justify-between bg-cover bg-center"
     >
+    <img src="./public/relaxed_reading.svg" alt=""  className="w-fit h-[500px]"/>
       <div className="flex flex-col ">
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-4xl font-bold text-[#0a0a0a]">
           Welcome to{" "}
-          <span className="text-8xl font-bold uppercase block">Rebook</span>
+          <span className="text-9xl font-bold uppercase block text-[#2563eb]">Rebook</span>
         </h1>
-        <p className="text-lg mt-2">
+        <p className="text-lg mt-2 text-[#1e1e1f]">
           Find, Explore, and Read—Books You Need at{" "}
-          <span className="text-2xl block font-semibold">Lightning Speed</span>
-          <span className="text-2xl font-semibold block">
+          <span className="text-2xl text-[#0a0a0a] block font-semibold">Lightning Speed</span>
+          <span className="text-2xl text-[#0a0a0a] font-semibold block">
             At Your Price
           </span>{" "}
-          <span className="text-2xl font-semibold"> Near You</span>
+          <span className="text-2xl text-[#0a0a0a] font-semibold"> Near You</span>
         </p>
         {!isLogined ? (
           <Link to={"/login"}>
